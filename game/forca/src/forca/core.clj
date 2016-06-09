0cfed0195259581af5e18250d4f060432605a49d
declau <dec82@hotmail.com>
2016-06-09 20:29:52 -0300

    remove map

--- game/forca/src/forca/core.clj
+++ game/forca/src/forca/core.clj
@@ -1,24 +1,31 @@
 (ns forca.core
   (:gen-class))
 
-	(def total-de-vidas 6)
 
-	(defn perdeu [] (println "Voce perdeu"))
+(def total-de-vidas 6)
 
-	(defn jogo [vidas palavra]
+(defn perdeu [] (print "Você perdeu"))
+(defn ganhou [] (print "Você ganhou!"))
 
-		(if (= vidas 0) 
-			(perdeu)
-			(do
-				(print vidas)
-				(jogo (dec vidas))
-			)
-		)
-	)
+(defn letras-faltantes [palavra acertos]
+    (remove (fn [letra] (contains? acertos (str letra))) palavra))
 
+(defn acertou-a-palavra-toda? [palavra acertos] 
+    (empty? (letras-faltantes palavra acertos))
+)
+
+(defn jogo [vidas palavra acertos]
+    (if (= vidas 0) 
+        (perdeu)
+        (if (acertou-a-palavra-toda? palavra acertos)
+            (ganhou)
+            (print "Chuta, amigo!")
+        )
+    )
+)
 
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println ""))
+  (println "Hello, World!"))
